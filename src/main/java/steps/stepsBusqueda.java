54e43136176c853ccd6a4371c15aa3571583516e
Juan Olivares <juanpabloolivares04@gmail.com>
2022-04-26 00:37:46 -0500

    Agrega Runner

--- src/main/java/steps/stepsBusqueda.java
+++ src/main/java/steps/stepsBusqueda.java
@@ -1,4 +1,18 @@
 package steps;
 
+import net.thucydides.core.annotations.Step;
+import pageObject.pageBusqueda;
+
 public class stepsBusqueda {
+
+    pageBusqueda objpageBusqueda;
+
+@Step
+public void ingresoNavegador(){
+objpageBusqueda.open();
+objpageBusqueda.maxPantalla();
+}
+
+
+
 }
--- src/test/java/runners/runnerBusqueda.java
+++ src/test/java/runners/runnerBusqueda.java
@@ -1,9 +1,11 @@
 package runners;
 
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.junit.runner.RunWith;
 
-@CucumberOptions(features = "src/test/resources/features/searchEbay.feature",glue = "definitions")
-public class runnerBusqueda {
+
+
+@CucumberOptions(features = "src/test/resources/features/searchEbay.feature", glue = "definitions")
+public class runnerBusqueda extends AbstractTestNGCucumberTests {
 }
