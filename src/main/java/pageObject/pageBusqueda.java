05d92f3306a7c288bd5a092ef06b38e9ff440f27
Juan Olivares <juanpabloolivares04@gmail.com>
2022-04-26 00:28:01 -0500

    Se agrega el runner

--- src/main/java/pageObject/pageBusqueda.java
+++ src/main/java/pageObject/pageBusqueda.java
@@ -9,6 +9,8 @@ import net.thucydides.core.pages.PageObject;
 public class pageBusqueda extends PageObject {
     String txtSearch="//input[@id='gh-ac']";
     String btnSearch="//input[@id='gh-btn']";
+    String chkAdidas="//input[@aria-label='adidas']";
+    String txtTotalSearch="//h1[@class='srp-controls__count-heading']/span[1]";
 
 
 }
--- src/test/java/runners/runnerBusqueda.java
+++ src/test/java/runners/runnerBusqueda.java
@@ -1,4 +1,9 @@
 package runners;
 
+
+import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@CucumberOptions(features = "src/test/resources/features/searchEbay.feature",glue = "definitions")
 public class runnerBusqueda {
 }
