feb46e656e288be9928046c3b42803db44fef512
Juan Olivares <juanpabloolivares04@gmail.com>
2022-04-26 00:47:41 -0500

    Agrega driverchrome

--- src/test/java/definitions/definitionBusqueda.java
+++ src/test/java/definitions/definitionBusqueda.java
@@ -6,25 +6,21 @@ import io.cucumber.java.es.Entonces;
 
 public class definitionBusqueda {
 
-    @Dado("que se requiere buscar")
+    @Dado("que se requiere buscar inventario de tenis adidas")
     public void que_se_requiere_buscar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Cuando("se busque por la palabra {string}")
     public void se_busque_por_la_palabra(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Cuando("filtre por {string}")
     public void filtre_por(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Entonces("se debe mostrar el numero de zapatos disponibles con el filtro aplicado")
     public void se_debe_mostrar_el_numero_de_zapatos_disponibles_con_el_filtro_aplicado() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 
--- src/test/resources/features/searchEbay.feature
+++ src/test/resources/features/searchEbay.feature
@@ -1,7 +1,7 @@
 #language: es
   Característica: Realizar una busqueda en la pagina ebay
     Escenario: Busqueda Ebay
-      Dado que se requiere buscar
+      Dado que se requiere buscar inventario de tenis adidas
       Cuando se busque por la palabra "shoes"
       Y filtre por "adidas"
       Entonces se debe mostrar el numero de zapatos disponibles con el filtro aplicado
